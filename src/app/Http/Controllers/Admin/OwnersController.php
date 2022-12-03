f4c844d7052d83e574bd64b29aa8918b1fd160f9
yankeno <kn5sttyryh@gmail.com>
2022-12-03 12:39:41 +0900

    [add]オーナー編集機能の追加

--- src/app/Http/Controllers/Admin/OwnersController.php
+++ src/app/Http/Controllers/Admin/OwnersController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Owner;
 use Carbon\Carbon;
+use App\Models\Owner;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class OwnersController extends Controller
 {
@@ -36,7 +38,7 @@ class OwnersController extends Controller
         // ]);
         // var_dump($q_first);
         // dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -58,7 +60,20 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index')
+            ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
@@ -80,7 +95,8 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -92,7 +108,14 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->email);
+        $owner->save();
+
+        return redirect()->route('admin.owners.index')
+            ->with('message', 'オーナー情報を更新しました。');
     }
 
     /**
--- src/resources/views/admin/owners/index.blade.php
+++ src/resources/views/admin/owners/index.blade.php
@@ -11,11 +11,12 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 mx-auto">
+                            <x-flash-message status="info" />
+                            <div class="flex justify-end mb-4">
+                                <button onclick="location.href='{{ route('admin.owners.create') }}'"
+                                    class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+                            </div>
                             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-                                <div class="flex justify-end mb-4">
-                                    <button onclick="location.href='{{ route('admin.owners.create') }}'"
-                                        class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
-                                </div>
                                 <table class="table-auto w-full text-left whitespace-no-wrap">
                                     <thead>
                                         <tr>
@@ -29,7 +30,7 @@
                                                 class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
                                                 作成日</th>
                                             <th
-                                                class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
                                         </tr>
                                     </thead>
@@ -39,8 +40,10 @@
                                                 <td class="px-4 py-3">{{ $owner->name }}</td>
                                                 <td class="px-4 py-3">{{ $owner->email }}</td>
                                                 <td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-                                                <td class="w-10 text-center">
-                                                    <input name="plan" type="radio">
+                                                <td class="px-4 py-3">
+                                                    <button
+                                                        onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
+                                                        class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded">編集</button>
                                                 </td>
                                             </tr>
                                         @endforeach
