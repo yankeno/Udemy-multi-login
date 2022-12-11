24a719f4dd4955f2708a3f276ae5866908ac2c30
yankeno <kn5sttyryh@gmail.com>
2022-12-11 12:43:59 +0900

    [update]商品詳細画面で在庫数量を表示

--- src/app/Http/Controllers/User/ItemController.php
+++ src/app/Http/Controllers/User/ItemController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\User;
 
-use Illuminate\View\View;
-use App\Http\Controllers\Controller;
+use App\Models\Stock;
 use App\Models\Product;
+use Illuminate\View\View;
 use App\Services\ItemService;
+use App\Http\Controllers\Controller;
 
 class ItemController extends Controller
 {
@@ -24,6 +25,14 @@ class ItemController extends Controller
     public function show($id): View
     {
         $product = Product::findOrFail($id);
-        return view('user.show', compact('product'));
+
+        $quantity =  Stock::where('product_id', $product->id)
+            ->sum('quantity');
+
+        if ($quantity > 9) {
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
     }
 }
